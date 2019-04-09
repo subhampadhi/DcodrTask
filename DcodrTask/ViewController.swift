247f34edfa27ce765fd59bb3761a9dc3d2cb374e
Subham Padhi <subhampadhi15@gmail.com>
2019-04-09 17:10:31 +0530

    Initial Commit

---
+++ DcodrTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DcodrTask
+//
+//  Created by Subham Padhi on 09/04/19.
+//  Copyright © 2019 Subham Padhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
